2c1f4ec34fb7726df026e484449981a7d66376b3
Maitray Bhatt <maitray.bhatt@unity3d.com>
2025-10-16 16:05:38 +0530

    Before looping logic

--- tests/smoke.spec.js
+++ tests/smoke.spec.js
@@ -5,6 +5,7 @@ import { DashboardPage } from '../pages/DashboardPage.js';
 import { TestData, PostCode } from '../Data/testData.js';
 
 
+/*
 test('Place Order End-to-End', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const orderPage = new OrderPage(page);
@@ -17,6 +18,7 @@ test('Place Order End-to-End', async ({ page }) => {
 
   //enter postcode
   await test.step('Enter postcode', async () => {
+    await loginPage.goto();
     await orderPage.enterPostcode(TestData.postcodes[0]);
   });
 
@@ -36,12 +38,69 @@ test('Place Order End-to-End', async ({ page }) => {
 
   //select skip & property
   await test.step('Select skip & property', async () => {
-    await orderPage.selectSkip(TestData.SkipSize[0]);
+    await orderPage.selectSkip(TestData.SkipSize[1]);
   });
 
  //Permit check
   await test.step('Permit check', async () => {
-    await orderPage.permitCheck(TestData.Placement[0]);
+    await orderPage.permitCheck(TestData.Placement[1]); //Private Property, Public Property, Grass verge, Not sure
+  });
+ 
+
+  await test.step('Choose date', async () => {
+    await orderPage.chooseDate(TestData.BookingDay[0]);
+  });
+
+  await test.step('Complete payment', async () => {
+    await orderPage.completePayment();
+  });
+
+  await test.step('Verify dashboard', async () => {
+    await dashboardPage.gotoSuccessPage();
+    await dashboardPage.verifyDashboard();
+  });
+});
+*/
+//orders in loop
+
+test('Place Order in loop', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const orderPage = new OrderPage(page);
+  const dashboardPage = new DashboardPage(page);
+
+  await test.step('Login to application', async () => {
+    await loginPage.goto();
+    await loginPage.login(TestData.credentials.username, TestData.credentials.password);
+  });
+
+  //enter postcode
+  await test.step('Enter postcode', async () => {
+    await loginPage.goto();
+    await orderPage.enterPostcode(TestData.postcodes[0]);
+  });
+
+  //select waste type options
+  await test.step('Select waste type', async () => {
+    await orderPage.selectWaste(TestData.WasteType[0]);
+    await orderPage.selectWaste(TestData.WasteType[1]);
+  //  await orderPage.selectWaste(TestData.WasteType[2]);
+   // await orderPage.selectWaste(TestData.WasteType[3]);
+ 
+  });
+
+  //select heavy waste & plasterboard options
+  await test.step('Continue waste type', async () => {
+    await orderPage.continueWaste(TestData.HeavyWaste[0], TestData.PlasterBoard[0]);
+  });
+
+  //select skip & property
+  await test.step('Select skip & property', async () => {
+    await orderPage.selectSkip(TestData.SkipSize[1]);
+  });
+
+ //Permit check
+  await test.step('Permit check', async () => {
+    await orderPage.permitCheck(TestData.Placement[1]); //Private Property, Public Property, Grass verge, Not sure
   });
  
 
