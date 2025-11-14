d5004e094f83e8c8f3556bc5cb820827bb33c992
Maitray MacBook Pro <maitraymacbookpro@Maitrays-MacBook-Pro.local>
2025-11-14 20:20:29 +0530

    code added for - add Tonne Bags- scenario

--- tests/smoke.spec.js
+++ tests/smoke.spec.js
@@ -9,7 +9,7 @@ import { TestData } from '../Data/TestData.js';
 
 const csvPath = './Data/testData.csv';
 
-test.describe('Change billing address and place order', () => {
+test.describe('Place order and add 2 Tonne bags', () => {
   test.setTimeout(180000); // 3 minutes
   let orderPage, dashboardPage, orderDeliverDetailsPage;
 
@@ -25,7 +25,7 @@ test.describe('Change billing address and place order', () => {
     test.page = page;
   });
 
-  test('Change billing address and place order', async () => {
+  test('Place order and add Tonne bags', async () => {
     const page = test.page;
     const orderPage = new OrderPage(page);
     const dashboardPage = new DashboardPage(page);
@@ -35,7 +35,7 @@ test.describe('Change billing address and place order', () => {
 
     // ✅ Get random CSV row at runtime
     const randomRow = getRandomRow(csvPath);
-    
+
 
 
     console.log(`🧾 Running Guest flow for:  ${TestData.postcodes[0]}, ${TestData.WasteType[0]}, Heavywaste -${TestData.HeavyWaste[0]}, Plasterboard -${TestData.PlasterBoard[0]}, Skipsize-${TestData.SkipSize[0]}, ${TestData.Placement[0]}`);
@@ -72,10 +72,92 @@ test.describe('Change billing address and place order', () => {
 
     await test.step('Guest registration', async () => {
       await signUpPage.fillSignUpFormExistingUser();
-      
+      await orderPage.completePayment();
     });
 
-  await test.step('Change billing address', async () => {
+    await test.step('Navigate to dashboard', async () => {
+      await dashboardPage.gotoSuccessPage();
+    });
+
+    await test.step('Verify Order Delivery Details', async () => {
+      await dashboardPage.navigateToViewOrderDetails();
+      await orderDeliverDetailsPage.verifyOrderDeliveryDetails();
+    });
+
+    await test.step('Add 2 Tonne Bags from order details', async () => {
+      await orderDeliverDetailsPage.addTonneBag();
+    });
+
+  });
+});
+
+test.describe('Change billing address and place order', () => {
+  test.setTimeout(180000); // 3 minutes
+  let orderPage, dashboardPage, orderDeliverDetailsPage;
+
+  test.beforeEach(async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    orderPage = new OrderPage(page);
+    dashboardPage = new DashboardPage(page);
+    orderDeliverDetailsPage = new OrderDeliveryDetailsPage(page);
+
+    test.context = context;
+    test.page = page;
+  });
+
+  test('Change billing address and place order', async () => {
+    const page = test.page;
+    const orderPage = new OrderPage(page);
+    const dashboardPage = new DashboardPage(page);
+    const loginPage = new LoginPage(page);
+    const signUpPage = new SignUpPage(page);
+    const orderDeliverDetailsPage = new OrderDeliveryDetailsPage(page);
+
+    // ✅ Get random CSV row at runtime
+    const randomRow = getRandomRow(csvPath);
+
+
+
+    console.log(`🧾 Running Guest flow for:  ${TestData.postcodes[0]}, ${TestData.WasteType[0]}, Heavywaste -${TestData.HeavyWaste[0]}, Plasterboard -${TestData.PlasterBoard[0]}, Skipsize-${TestData.SkipSize[0]}, ${TestData.Placement[0]}`);
+
+    await loginPage.goto();
+
+    await test.step('Enter postcode', async () => {
+      await orderPage.enterPostcode(TestData.postcodes[0]);
+    });
+
+    await test.step('Select waste type', async () => {
+      await orderPage.selectWaste(TestData.WasteType[0]);
+    });
+
+    await test.step('Continue waste type', async () => {
+      await orderPage.continueWaste(TestData.HeavyWaste[0], TestData.PlasterBoard[0]);
+    });
+
+    await test.step('Select skip & property', async () => {
+      await orderPage.selectSkip(TestData.SkipSize[0], TestData.PlasterBoard[0], "No", "No");
+    });
+
+    await test.step('Permit check', async () => {
+      await orderPage.permitCheck(TestData.Placement[0]);
+    });
+
+    await test.step('Choose date', async () => {
+      await orderPage.chooseDate(TestData.BookingDay[0]);
+    });
+
+    await test.step('Complete payment', async () => {
+      await orderPage.completePayment();
+    });
+
+    await test.step('Guest registration', async () => {
+      await signUpPage.fillSignUpFormExistingUser();
+
+    });
+
+    await test.step('Change billing address', async () => {
       await orderPage.changeBillingAddress();
       await orderPage.completePayment();
     });
@@ -122,7 +204,7 @@ test.describe('Place order and add Road permit', () => {
 
     // ✅ Get random CSV row at runtime
     const randomRow = getRandomRow(csvPath);
-    
+
 
 
     console.log(`🧾 Running Guest flow for:  ${TestData.postcodes[0]}, ${TestData.WasteType[0]}, Heavywaste -${TestData.HeavyWaste[0]}, Plasterboard -${TestData.PlasterBoard[0]}, Skipsize-${TestData.SkipSize[0]}, ${TestData.Placement[0]}`);
@@ -204,7 +286,7 @@ test.describe('Start order as guest and logs in with existing account', () => {
 
     // ✅ Get random CSV row at runtime
     const randomRow = getRandomRow(csvPath);
-    
+
 
 
     console.log(`🧾 Running Guest flow for:  ${randomRow.Postcodes}, ${randomRow.WasteType}, Heavywaste -${randomRow.HeavyWaste}, Plasterboard -${randomRow.PlasterBoard}, Skipsize-${randomRow.SkipSize}, ${randomRow.Placement}`);
@@ -281,7 +363,7 @@ test.describe('Place an order as Guest User', () => {
 
     // ✅ Get random CSV row at runtime
     const randomRow = getRandomRow(csvPath);
-    
+
 
 
     console.log(`🧾 Running Guest flow for:  ${randomRow.Postcodes}, ${randomRow.WasteType}, Heavywaste -${randomRow.HeavyWaste}, Plasterboard -${randomRow.PlasterBoard}, Skipsize-${randomRow.SkipSize}, ${randomRow.Placement}`);
